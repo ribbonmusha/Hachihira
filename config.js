2d67ea2580ab54923f2f1c967e149e7727c47f1c
mhmmd1444 <mhmmd1444@github.com>
2022-06-27 20:11:12 +0700

    Adding the source code privately

---
+++ config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    app: {
+        px: 'n!',
+        token: 'INSERT YOUR BOT TOKEN',
+        playing: 'BRUH'
+    },
+
+    opt: {
+        DJ: {
+            enabled: false,
+            roleName: 'DJ',
+            commands: ['back', 'clear', 'filter', 'loop', 'pause', 'resume', 'seek', 'shuffle', 'skip', 'stop', 'volume']
+        },
+        maxVol: 100,
+        loopMessage: false,
+        discordPlayer: {
+            ytdlOptions: {
+                quality: 'highestaudio',
+                highWaterMark: 1 << 25
+            }
+        }
+    }
+};
